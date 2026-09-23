cc7d92b7f7cd7ce3f4f2e059940c1b156f0f2d69
Michel Mayoral <turbotirek@gmail.com>
2026-09-23 15:00:50 -0600

    Versión_03: Cambio ascendente y descendente

--- Arreglos.UI.consola/Program.cs
+++ Arreglos.UI.consola/Program.cs
@@ -1,8 +1,27 @@
-﻿internal class Program
+﻿using Arreglos.Logica;
+using System.Collections.Specialized;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("\nArreglos");
+
+        MiArreglo oMiArreglo = new MiArreglo(10);
+
+        oMiArreglo.Llenar(5, 20);
+
+
+        Console.WriteLine("\nDesordenado");
+        Console.WriteLine(oMiArreglo);
+
+        Console.WriteLine("\nordenado ascendente");
+        oMiArreglo.Ordenar();
+        Console.WriteLine(oMiArreglo);
+
+        Console.WriteLine("\nordenado descendente");
+        oMiArreglo.Ordenar(false);
+        Console.WriteLine(oMiArreglo);
 
 
         Console.ReadKey();
